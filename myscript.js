cbd4992d2a6f47c5b4a37b98f0c0fb42bef18e7e
ajeeshakshay <ajeeshakshay8@gmail.com>
2024-11-02 23:29:28 -0400

    Updates in JS

--- myscript.js
+++ myscript.js
@@ -8,53 +8,84 @@ const lnameWarning = document.querySelector(".lname_warning")
 
 const emailText = document.querySelector("#email_text");
 const fieldWarningEmail = document.querySelector(".field-warning-email2");
-
+const fieldWarningEmail1 = document.querySelector(".field-warning-email");
 const alertMessage = document.querySelector(".alert_message");
 
-let isValid=true
+const fieldWarningRadio = document.querySelector(".field-warning-radio")
+
+let isfname=true;
+let islname=true;
+let isemail=true;
+let isradio=true;
+
+const validateEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
+
 submitButton.addEventListener("click",function(){
     
     if (fnameText.value=="")
     {
         fnameWarning.classList.remove("hidden");
-        isValid=false
+        isfname=false
     }
     else
     {
         fnameWarning.classList.add("hidden");
-        isValid=true
+        isfname=true
     }
     
     if (lnameText.value=="")
     {
         lnameWarning.classList.remove("hidden"); 
-        isValid=false
-        
+        islname=false  
     }
     else
     {
         lnameWarning.classList.add("hidden");
-        isValid=true
+        islname=true
     }
 
     if (emailText.value=="")
     {
         fieldWarningEmail.classList.remove("hidden"); 
-        isValid=false
+        isemail=false
+    }
+    else if(!validateEmail(emailText.value))
+    {
+        fieldWarningEmail.classList.add("hidden"); 
+        fieldWarningEmail1.classList.remove("hidden"); 
+        isemail=false
+        
     }
     else
     {
+        fieldWarningEmail1.classList.add("hidden"); 
         fieldWarningEmail.classList.add("hidden");
-        isValid=true
+        isemail=true
+        
+    }
+
+    if(document.querySelectorAll('input[type="radio"]:checked').length===0) {
+        fieldWarningRadio.classList.remove("hidden")
+        isradio=false
+    }
+    else{
+        fieldWarningRadio.classList.add("hidden")
+        isradio=true
     }
         
-           
+    if(isfname && islname && isemail && isradio)
+        {
+            alertMessage.classList.remove("hidden");
+            setTimeout(function () {
+                alertMessage.classList.add("hidden");
+            }, 1000);
+        }     
 })
 
-if(isValid==true)
-    {
-        alertMessage.classList.remove("hidden");
-        setTimeout(function () {
-            alertMessage.classList.add("hidden");
-        }, 1000);
-    } 
+
